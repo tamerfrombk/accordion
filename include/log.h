397fdfbd75eee12c119acf758f0da55851955e70
Tamer Aly <taly.dev75@gmail.com>
2021-03-18 18:26:16 -0400

    introduce ACC_LOG_IMPL to wrap macro

--- include/log.h
+++ include/log.h
@@ -6,22 +6,19 @@
 
 #define ACC_LOG_MAX_BUFFER_LENGTH (255)
 
+#define ACC_LOG_IMPL(prefix, str, ...) do {\
+        char debug_buf[ACC_LOG_MAX_BUFFER_LENGTH + 1] = {0};\
+        strcpy(debug_buf, prefix);\
+        strncat(debug_buf, str, ACC_LOG_MAX_BUFFER_LENGTH - strlen(prefix));\
+        fprintf(stderr, debug_buf,##__VA_ARGS__);\
+    } while (0)
+
 #ifdef NDEBUG
     #define debug(str, ...)
 #else
-    #define debug(str, ...) do {\
-        char debug_buf[ACC_LOG_MAX_BUFFER_LENGTH + 1] = {0};\
-        strcpy(debug_buf, "DEBUG: ");\
-        strncat(debug_buf, str, ACC_LOG_MAX_BUFFER_LENGTH - strlen("DEBUG: "));\
-        fprintf(stderr, debug_buf,##__VA_ARGS__);\
-    } while (0)
+    #define debug(str, ...) ACC_LOG_IMPL("DEBUG: ", str,##__VA_ARGS__)
 #endif
 
-#define error(str, ...) do {\
-    fprintf(stderr, str,##__VA_ARGS__);\
-} while (0)
+#define error(str, ...) ACC_LOG_IMPL("ERROR: ", str,##__VA_ARGS__)
 
-#define fatal(str, ...) do {\
-    fprintf(stderr, str,##__VA_ARGS__);\
-    exit(EXIT_FAILURE); \
-} while (0)
+#define fatal(str, ...) ACC_LOG_IMPL("FATAL: ", str,##__VA_ARGS__)
