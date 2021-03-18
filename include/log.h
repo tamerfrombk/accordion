dfd90e834e923b1cc9db593bf958a73473a168d3
Tamer Aly <taly.dev75@gmail.com>
2021-03-18 18:23:23 -0400

    add maximum length of log buffer

--- include/log.h
+++ include/log.h
@@ -4,13 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ACC_LOG_MAX_BUFFER_LENGTH (255)
+
 #ifdef NDEBUG
     #define debug(str, ...)
 #else
     #define debug(str, ...) do {\
-        char debug_buf[256] = {0};\
+        char debug_buf[ACC_LOG_MAX_BUFFER_LENGTH + 1] = {0};\
         strcpy(debug_buf, "DEBUG: ");\
-        strncat(debug_buf, str, sizeof(debug_buf) - 7 - 1);\
+        strncat(debug_buf, str, ACC_LOG_MAX_BUFFER_LENGTH - strlen("DEBUG: "));\
         fprintf(stderr, debug_buf,##__VA_ARGS__);\
     } while (0)
 #endif
